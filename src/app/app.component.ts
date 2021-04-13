972197d54897876b7b8d16c8bd1f246a72b1c197
0xMagicianⓁ <magicianlee007@gmail.com>
2021-04-13 12:04:01 +0400

    feat: show usdc, idle_usdc, ib3crv_gauge balance of FarmBossUSDC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,30 @@
 import { Component } from '@angular/core';
-import { IB3CRV } from '../constants/contractAddresses';
-import ib3CRV from '../constants/ib3CRV.json';
+import {
+  IB3CRV_GAUGE,
+  IDLE_USDC,
+  USDC,
+  FarmBoss_USDC,
+} from '../constants/contractAddresses';
+import ib3CRV_GAUGE from '../constants/ib3CRV_GAUGE.json';
+import idle_USDC from '../constants/idle_USDC.json';
+import usdc from '../constants/usdc.json';
 
 let Web3: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ayf-management-dashboard';
   web3: any;
-  ib3CRV: any;
-  ib3CRVHolder: string;
-  ib3CRVBalance: any;
+  ib3CRV_GAUGE: any;
+  idle_USDC: any;
+  usdc: any;
+  fbUSDC_Balance = { usdc: '0', ib3CRV_Gauge: '0', idle_USDC: '0' };
+
   constructor() {
-    this.ib3CRVHolder = '0x661bf31c780c37764f27f28b195f2b7e973c3c01';
-    this.ib3CRVBalance = 0
     if (!window['Web3']) {
       this.injectScript();
     } else {
@@ -38,7 +46,9 @@ export class AppComponent {
   generateProvider() {
     Web3 = window['Web3'];
     this.web3 = new Web3(
-      new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/f0f45988269240c1a220bd21fe5ea02a')
+      new Web3.providers.HttpProvider(
+        'https://mainnet.infura.io/v3/f0f45988269240c1a220bd21fe5ea02a'
+      )
     );
     console.log(this.web3);
   }
@@ -50,11 +60,30 @@ export class AppComponent {
     console.error('web3 error injected');
   }
   async initContract() {
-    this.ib3CRV = new this.web3.eth.Contract(
-      ib3CRV,
-      IB3CRV
-    )
-    const balanceInWei = await this.ib3CRV.methods.balanceOf(this.ib3CRVHolder).call();
-    this.ib3CRVBalance = this.web3.utils.fromWei(balanceInWei);
+    // Create ib3CRV_GAUGE, USDC, idle_USDC contract
+
+    this.ib3CRV_GAUGE = new this.web3.eth.Contract(ib3CRV_GAUGE, IB3CRV_GAUGE);
+
+    this.idle_USDC = new this.web3.eth.Contract(idle_USDC, IDLE_USDC);
+
+    this.usdc = new this.web3.eth.Contract(usdc, USDC);
+
+    // Get the balance of FarmBoss_USDC
+    const usdcBalanceWei = await this.usdc.methods
+      .balanceOf(FarmBoss_USDC)
+      .call();
+    const usdcDecimals = await this.usdc.methods.decimals().call();
+    console.log(usdcBalanceWei);
+    this.fbUSDC_Balance.usdc = this.web3.utils.fromWei(usdcBalanceWei, 'mwei');
+    const idleUSDCBalanceWei = await this.idle_USDC.methods
+      .balanceOf(FarmBoss_USDC)
+      .call();
+    this.fbUSDC_Balance.idle_USDC = this.web3.utils.fromWei(idleUSDCBalanceWei);
+    const ib3CRV_GaugeBalanceWei = await this.ib3CRV_GAUGE.methods
+      .balanceOf(FarmBoss_USDC)
+      .call();
+    this.fbUSDC_Balance.ib3CRV_Gauge = this.web3.utils.fromWei(
+      ib3CRV_GaugeBalanceWei
+    );
   }
 }
